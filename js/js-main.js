8b1bf49a20854778b5991498d8f67dedcd61f38f
Sophia <swoozie14.com>
2016-09-23 15:23:25 -0700

    Add rainbow color and gradient black options for sketch pad

--- js/js-main.js
+++ js/js-main.js
@@ -1,18 +1,50 @@
+var colorType = "default";
+
 $(document).ready(function() {
-    create_table(16);
-    color_default();
+    createTable(16);
+    if (colorType === "default") {
+        colorDefault();
+    }
 
     // Resets the sketch pad when user clicks "Clear Sketch Pad" button
     $("#clear").click(function() {
-        $("td").removeClass("colored");
+        $("td").removeAttr("style");
     });
 
     // Creates a new sketch pad with user-prompted size when user clicks
     // "New Sketch Pad" button
     $("#new-pad").click(function() {
-        new_sketch();
-        color_default();
+        newSketch();
+        if (colorType === "default") {
+            colorDefault();
+        }   
+        else if (colorType === "rainbow") {
+            rainbowColors();
+        }
+        else {
+            gradientBlack();
+        }
     });
+
+    // Makes colors rainbow when user clicks "Rainbow Colors" button
+    $("#rainbow").click(function() {
+        colorType = "rainbow";
+        rainbowColors();
+    });
+
+    // Makes colors gradient black when user clicks "Gradient Black" button
+    $("#gradient").click(function() {
+        colorType = "gradient";
+        gradientBlack();
+        $("td").addClass("startGradient");
+    });
+
+    // Makes colors default black when user clicks "Default Black" button
+    $("#default").click(function() {
+        colorType = "default";
+        colorDefault();
+    });
+
 });
 
 
@@ -21,7 +53,7 @@ $(document).ready(function() {
 //***************************************************//
 
 // Creates a table with num x num dimensions in the wrapper div
-function create_table(num) {
+function createTable(num) {
     $(".wrapper").append("<table></table>");
     for (var i = 0; i < num; i++) {
         $("table").append("<tr ID='row" + i + "' ");
@@ -30,29 +62,49 @@ function create_table(num) {
         }
         $("table").append("</tr>");
     }
-    var squareSize = Math.floor(100/num) + "%";
-    $("td").css({"width": squareSize, "height": squareSize});
 }
 
 // When user mouses over a box, colors it the default black color
-function color_default() {
+function colorDefault() {
     $("td").mouseover(function() {
-        $(this).addClass("colored");
+        $(this).css("background-color", "black");
+        //$(this).css("opacity", "1.0");
     });
-
 }
 
-// When user clicks "New Sketch Pad" button, creates a new sketch pad
-// with the user-prompted size
-function new_sketch() {
+// Creates a new sketch pad with the user-prompted size
+function newSketch() {
     var input = prompt("Please enter a grid size:");
     if (input === null) {
         return;
     }
     $("td").removeClass("colored");
         $("table").remove();
-        create_table(input);
+        createTable(input);
 }
 
+// Changes colors of hovering to randomized rainbow
+function rainbowColors() {
+    $("td").mouseover(function() {
+        var newColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+        $(this).css("background-color", newColor);
+    });
 
+};
+
+// Changes colors of hovering to gradient black (darker each time the
+// user hovers over the same box)
+function gradientBlack() {
+    $("td").mouseover(function() {
+        if (!$(this).hasClass("startGradient") && 
+        $(this).css("opacity") > 0) {
+            $(this).css("opacity", "+=0.1");
+        }
+        if ($(this).hasClass("startGradient")) {
+            $(this).css("background-color", "black");
+            $(this).css("opacity", "0.1");
+            $(this).removeClass("startGradient");
+        }
+    });
+}
 
