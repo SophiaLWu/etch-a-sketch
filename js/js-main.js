705db94b2860a8d7ba6fb5b18a9b4c128d087159
Sophia <swoozie14.com>
2016-09-23 12:12:15 -0700

    Fix problem with changing grid size and add header/footer/colors

--- js/js-main.js
+++ js/js-main.js
@@ -1,11 +1,6 @@
 $(document).ready(function() {
-    // Creates a default 16x16 sketch pad
     create_table(16);
-
-    // Colors box when user hovers over it
-    $("td").hover(function() {
-        $(this).addClass("colored");
-    });
+    color_default();
 
     // Resets the sketch pad when user clicks "Clear Sketch Pad" button
     $("#clear").click(function() {
@@ -15,12 +10,17 @@ $(document).ready(function() {
     // Creates a new sketch pad with user-prompted size when user clicks
     // "New Sketch Pad" button
     $("#new-pad").click(function() {
-        var gridSize = prompt("Please enter a grid size: "); 
+        new_sketch();
+        color_default();
     });
 });
 
 
-// Creates a table with num x num dimensions within the wrapper div
+//***************************************************//
+//******************** FUNCTIONS ********************//
+//***************************************************//
+
+// Creates a table with num x num dimensions in the wrapper div
 function create_table(num) {
     $(".wrapper").append("<table></table>");
     for (var i = 0; i < num; i++) {
@@ -30,4 +30,29 @@ function create_table(num) {
         }
         $("table").append("</tr>");
     }
+    var squareSize = Math.floor(100/num) + "%";
+    $("td").css({"width": squareSize, "height": squareSize});
 }
+
+// When user mouses over a box, colors it the default black color
+function color_default() {
+    $("td").mouseover(function() {
+        $(this).addClass("colored");
+    });
+
+}
+
+// When user clicks "New Sketch Pad" button, creates a new sketch pad
+// with the user-prompted size
+function new_sketch() {
+    var input = prompt("Please enter a grid size:");
+    if (input === null) {
+        return;
+    }
+    $("td").removeClass("colored");
+        $("table").remove();
+        create_table(input);
+}
+
+
+
